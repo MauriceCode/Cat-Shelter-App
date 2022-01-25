fa9e57403d4422b199ea6e346e7941f5547a3085
Maurice Moore <mauricemoore.fit@gmail.com>
2022-01-24 18:05:26 -0600

    First Commit

---
+++ resources/scripts/app.js
@@ -0,0 +1,12 @@
+const colors = require('colors');
+
+console.log('hello'.blue)
+
+import { doc, setDoc } from "firebase/firestore"; 
+
+// Add a new document in collection "cities"
+await setDoc(doc(db, "cities", "LA"), {
+  name: "Los Angeles",
+  state: "CA",
+  country: "USA"
+});
---
+++ resources/scripts/firebase.js
@@ -0,0 +1,7 @@
+// with Commonjs syntax (if using Node)
+const firebase = require("firebase/app");
+require("firebase/firestore");
+
+// with ES Modules (if using client-side JS, like React)
+import firebase from 'firebase/app';
+import 'firebase/firestore';
